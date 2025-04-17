22478e17b1ed29721966d1c326887ee50ed8aac3
Asfand0306 <166415318+Asfand0306@users.noreply.github.com>
2025-04-17 16:29:37 -0600

    css changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
 const Page = () => {
+
+
+
+
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-white">
-      <h1 className="text-5xl font-extrabold mb-4">Trackchieve</h1>
+      <h1 className="text-7xl font-extrabold  mb-4">Trackchieve</h1>
       <h2 className="text-2xl font-semibold mb-2">
          Track your Steam Achievements
       </h2>
@@ -11,15 +15,15 @@ const Page = () => {
         Unlock your gaming accomplishments: View, track, and manage your Steam achievements with ease.
       </p>
 
-      <div className="bg-primary-dark p-6 rounded-2xl border border-purple-700 shadow-lg">
-        <h3 className="text-lg font-semibold mb-4 text-center">Sign in</h3>
+      <div className="bg-background p-6 rounded-2xl border border-primary shadow-lg">
+        <h3 className="text-lg font-Inter mb-4 text-center">Sign in</h3>
         
-        <button className="flex items-center justify-center w-full mb-4 bg-purple-700 hover:bg-purple-800 text-white font-medium py-2 rounded-lg transition">
-          <img src="./Steam_icon.png" alt="Steam" className="h-5 w-5 mr-2" />
+        <button className="flex items-center justify-center w-full mb-4 p-3 border border-primary  hover:bg-purple-800 text-white font-medium py-2 rounded-lg transition">
+          <img src="/assets/Steam_icon.png" alt="Steam" className="h-6 w-6 mr-2"/>
           Steam Sign in
         </button>
         
-        <button className="w-full bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 rounded-lg transition">
+        <button className="flex items-center justify-center w-full border border-primary hover:bg-purple-700 text-white font-medium py-2 rounded-lg transition">
           Guest Login
         </button>
       </div>
